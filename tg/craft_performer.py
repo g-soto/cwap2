d36c18080f3b547fdd365a6d6ca687e964621ef4
g-soto <gsotogomez94@gmail.com>
2023-01-01 16:12:55 -0600

    only new weather update

--- tg/craft_performer.py
+++ tg/craft_performer.py
@@ -6,13 +6,10 @@ class CraftPerformer:
         self.post_message = post_message_with_author
         self.pin_message = post_and_pin_message_with_author
         self.subs = [("new_craft", self.new_craft), ("new_weather", self.new_weather_report)]
+        self.last_weather = None
 
     async def new_craft(self, message):
         await self.post_message(self.GURU_CHANNEL, message)
 
     async def new_weather_report(self, message):
-        if await self.is_new_weather(message):
-            await self.pin_message(self.GURU_CHANNEL, message)
-
-    async def is_new_weather(self, message):
-        return True
+        await self.pin_message(self.GURU_CHANNEL, message)
--- tg/cw_watcher.py
+++ tg/cw_watcher.py
@@ -1,4 +1,13 @@
 from telethon import events
+import regex as re
+
+weather_re = re.compile("In Chat Wars world now\n"
+                        "(\\W+)\\w+\n"
+                        "[0-9:]+\\n"
+                        "\\d+ \\w+ \\d+\n"
+                        "\n"
+                        "Weather forecast\n"
+                        "(.+)→(.+)\n")
 
 class CWWatcher:
     
@@ -7,19 +16,26 @@ class CWWatcher:
     def __init__(self, inform_event: callable) -> None:
         self.inform_event = inform_event
         self.watchs = [self.watch_for_craft, self.watch_for_time]
+        self.last_weather = None
         
     @events.register(events.NewMessage(outgoing=False, chats=CW_BOT))
     async def watch_for_craft(self, event):
         if self.is_craft_message(event):
             await self.inform_event("new_craft", event.message)
-
+            
     def is_craft_message(self, message):
         return 'has ordered' in message.raw_text
 
     @events.register(events.NewMessage(outgoing=False, chats=CW_BOT))
     async def watch_for_time(self, event):
-        if self.is_weather_message(event):
+        if self.is_new_weather_message(event):
             await self.inform_event("new_weather", event.message)
 
-    def is_weather_message(self, message):
-        return "Weather forecast" in message.raw_text
+    def is_new_weather_message(self, message):
+        if not (m := re.match(weather_re, message.raw_text)):
+            return False
+        new_weather = m.groups()
+        if new_weather == self.last_weather:
+            return False
+        self.last_weather = new_weather
+        return True
